44856a4f12c5b6fec386bf6746dded0f699e6cee
Panxoat <a20304645@gmail.com>
2019-07-11 11:57:15 +0900

    ver 3.1

--- script/dodo.js
+++ script/dodo.js
@@ -1,14 +1,17 @@
 const tag = '<button id = Done >Finish!</button>'
-const input_tag = '<input type = "text" id = post_out name = "todo" placeholder = "TODO" autocomplete="off">';
-
+const input_tag = '<input type = "text" id = post_out name = "todo" placeholder = "Write Down" autocomplete="off">';
+const fin_tag = '<p id=fin>Great !</p>'
 $(document).ready(()=>{
-    $('#todo_id').click(()=>{
+    $('#todo_id').click(()=>{ //Finish 클릭 시,
         $.ajax({
             url: '/api/post/db_delete',
             dataType:'json',
             type: 'POST'
         })
-        alert("GREAT!");
+        $('#fin_div').append(fin_tag);
+        $('#fin').fadeIn(500, setTimeout(() => {
+            $('#fin').fadeOut(500);
+        }, 1000));
         $('#input_id').append(input_tag);
         $('#Done').fadeOut("normal", ()=>{
             $('#Done').remove();
@@ -18,7 +21,7 @@ $(document).ready(()=>{
 })
 
 $(document).ready(()=>{
-    $('#input_id').keydown((key)=>{
+    $('#input_id').keydown((key)=>{ //input Write Down 시,
         if(key.keyCode == 13){
             $.ajax({
                 url: '/api/post',
